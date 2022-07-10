c9ae913d86f66456ef31b01431b6a662934942bd
tramy nguyen <tramy.nguyen@student-htw-berlin.de>
2022-07-10 19:02:24 +0200

    init

---
+++ src/main/java/webtech/com/dazzling/DazzlingApplication.java
@@ -0,0 +1,13 @@
+package webtech.com.dazzling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazzlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DazzlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webtech/com/dazzling/DazzlingApplicationTests.java
@@ -0,0 +1,13 @@
+package webtech.com.dazzling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DazzlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
