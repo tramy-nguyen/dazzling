e924f009228b94bfb2c40a33a9dbc44364988209
tramy nguyen <tramy.nguyen@student-htw-berlin.de>
2022-07-11 11:13:51 +0200

    modified application.properties

--- src/main/java/webtech/com/dazzling/api/Person.java
+++ src/main/java/webtech/com/dazzling/api/Person.java
@@ -1,2 +1,61 @@
-package webtech.com.dazzling.api;public class Person {
+package webtech.com.dazzling.api;
+
+public class Person {
+
+
+    private long id;
+    private String firstname;
+    private String lastname;
+    private boolean vaccinated;
+    private int age;
+
+    public Person (long id, String firstname, String lastname, int age, Boolean vaccinated) {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+            this.vaccinated = vaccinated;
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public boolean isVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
--- src/main/java/webtech/com/dazzling/web/PersonRestController.java
+++ src/main/java/webtech/com/dazzling/web/PersonRestController.java
@@ -1,2 +1,32 @@
-package webtech.com.dazzling.web;public class PersonRestController {
+package webtech.com.dazzling.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import webtech.com.dazzling.api.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonRestController {
+
+    private List<Person> persons;
+
+    public PersonRestController() {
+        persons = new ArrayList<>();
+        persons.add(new Person(1, "Max", "Mustermann", 20, true));
+        persons.add(new Person(2, "Emma", "Mustermann", 20, false));
+    }
+
+    @GetMapping(path = "/api/v1/persons")
+    public ResponseEntity<List <Person>> fetchPersons() {
+        return ResponseEntity.ok(persons);
+    }
+
+
+
+
+
 }
